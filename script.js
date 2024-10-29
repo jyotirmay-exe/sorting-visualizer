5cf5b65a580db9f72962fe89b2d6ad20cc47d406
Jyotirmay <jyotirmay18.jk@gmail.com>
2024-10-29 21:41:33 +0530

    bars

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = updateRange();
 
-var size=0;
-var delay=0;
+var size = 0;
+var delay = 0;
 
 document.addEventListener("DOMContentLoaded", (event) => {
     updateRange();
@@ -19,15 +19,28 @@ function updateSize() {
     value = Math.round(value / 2) * 2;
 
     slider.value = value;
-    size=value;
+    size = value;
     document.getElementById("sizeVal").innerText = slider.value;
+
+    const maxHeight = 250;
+    const unitHeight = maxHeight / value;
+
+    let arr = [];
+    for (let i = 1; i <= value; i++) {
+        arr.push(i * unitHeight);
+    }
+
+    document.querySelector('.bars').innerHTML = "";
+    for (let i = 0; i < size; i++) {
+        document.querySelector('.bars').innerHTML += `<div class='bar' style="height:${arr[i]}px;"></div>`;
+    }
 }
 
 function updateSpeed() {
     const slider = document.querySelector(".speedslider");
     let value = parseInt(slider.value);
     value = Math.round(value / 100) * 100;
-    delay=value;
+    delay = value;
     slider.value = value;
-    document.getElementById("speedVal").innerText = slider.value+"ms";
+    document.getElementById("speedVal").innerText = slider.value + "ms";
 }
