71e1fd1ad458330bd8f2d32bbf15eaac9c84b494
Jyotirmay <jyotirmay18.jk@gmail.com>
2024-11-23 22:05:42 +0530

    algos implementation

---
+++ scripts/insertionSort.js
@@ -0,0 +1,37 @@
+async function insertionSort() {
+    for (let i = 1; i < size; i++) {
+        let keyHeight = getHeight(i);
+        let j = i - 1;
+        if (stopflag) { return; }
+        bars[i].style.backgroundColor = colRed;
+        await sleep(250);
+        playclicksound();
+
+        while (j >= 0 && getHeight(j) > keyHeight) {
+            if (stopflag) { return; }
+            await sleep(delay);
+            bars[j].style.backgroundColor = colYellow;
+            bars[j + 1].style.backgroundColor = colGreen;
+            bars[j + 1].style.height = getHeight(j) + "px";
+            playswapsound();
+            bars[j].style.backgroundColor = colBlue;
+            bars[j + 1].style.backgroundColor = colBlue;
+            j--;
+        }
+        bars[j + 1].style.backgroundColor = colGreen;
+        bars[j + 1].style.height = keyHeight + "px";
+        await sleep(500);
+        bars[j + 1].style.backgroundColor = colBlue;
+        bars[i].style.backgroundColor = colBlue;
+    }
+
+    playfinishsound();
+    for (let i = 0; i < size; i++) {
+        await sleep(30);
+        bars[i].style.backgroundColor = colGreen;
+    }
+    for (let i = 0; i < size; i++) {
+        await sleep(30);
+        bars[i].style.backgroundColor = colBlue;
+    }
+}
--- scripts/selectionSort.js
+++ scripts/selectionSort.js
@@ -1,12 +1,10 @@
-function getHeight(i) {
-    return parseFloat(bars[i].style.height);
-}
-
 async function selectionSort() {    
     for(let i=0; i<size-1; i++) {
         bars[i].style.backgroundColor = colRed;
         let smol = i, prevsmol=smol;
         for(let j=i+1; j<size; j++) {
+            if(stopflag) { return; }
+            playclicksound();
             bars[j].style.backgroundColor = colYellow;
 
             await sleep(delay);
@@ -14,36 +12,34 @@ async function selectionSort() {
             if(getHeight(j) < getHeight(smol)) {
                 prevsmol = smol;
                 smol=j;
-                bars[smol].style.backgroundColor = colGreen;
             }
-
-            bars[j].style.backgroundColor = colBlue;
-            
-            if(prevsmol!=i)
+            if(prevsmol!=smol && prevsmol!=i)
                 bars[prevsmol].style.backgroundColor = colBlue;
+            if(j!=smol)
+                bars[j].style.backgroundColor = colBlue;
+            else
+                bars[j].style.backgroundColor = colGreen;
         }
-
         if(i!=smol) {
+            playswapsound();
             bars[i].style.backgroundColor = colGreen;
             bars[smol].style.backgroundColor = colGreen;
-
+            await sleep(250);
             let temp = getHeight(i);
             bars[i].style.height = getHeight(smol)+"px";
             bars[smol].style.height = temp+"px";
         }
-        await sleep(500);
+        await sleep(250);
         bars[smol].style.backgroundColor = colBlue;
         bars[i].style.backgroundColor = colBlue;
     }
-
+    playfinishsound();
     for(let i=0; i<size; i++) {
-        await sleep(delay/5);
+        await sleep(30);
         bars[i].style.backgroundColor = colGreen;
     }
-    
     for(let i=0; i<size; i++) {
-        await sleep(delay/5);
+        await sleep(30);
         bars[i].style.backgroundColor = colBlue;
     }
-
 }
